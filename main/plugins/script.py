b8cf48d99f4afd67b0e5f6a55223249e7c1fdd14
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-01 10:35:24 +0530

    Update script.py

--- main/plugins/script.py
+++ main/plugins/script.py
@@ -1,6 +1,6 @@
 class Translation(object):
 
-    START_TEXT = 'Hi👋, I am Save Restricted Content Bot\n\n**•FROM PUBLIC CHANNELS**\n-Send direct message/video link to clone it here.\ne.g:- `https://t.me/b/channelusername/43`\n\n🚨`NOTE:-` Our bot does not support \nPRIVATE CHANNEL/GROUP.\n\nJoin for update:- @Save_Restricted_contentz\n🥂kindly /donate for service alive' 
+    START_TEXT = 'Hi👋, I am Save Restricted Content Bot\n\n**•FROM PUBLIC CHANNELS**\n-Send direct message/video link to clone it here.\ne.g:- `https://t.me/channelusername/43`\n\n🚨`NOTE:-` Our bot does not support \nPRIVATE CHANNEL/GROUP.\n\nJoin for update:- @Save_Restricted_contentz\n🥂kindly /donate for service alive' 
     WAIT_MSG = """"<b>Processing ...</b>"""
     REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
     Donate = """
