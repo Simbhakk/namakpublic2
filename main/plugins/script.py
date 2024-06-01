4a7ff2ec14f58ae03c72a1775b10e671b702ebe4
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-01 10:00:36 +0530

    Update script.py

--- main/plugins/script.py
+++ main/plugins/script.py
@@ -1,8 +1,8 @@
 class Translation(object):
 
-    START_TEXT = 'Hi👋 I am Save Restricted Content Bot\n\n**•FROM PUBLIC CHANNELS**\n-Send direct message/video link to clone it here.\n\n🚨`NOTE:-` Our bot does not support \nPRIVATE CHANNEL/GROUP.\n\nJoin for update:- @Save_Restricted_contentz\n🥂kindly /donate for service alive' 
+    START_TEXT = 'Hi👋, I am Save Restricted Content Bot\n\n**•FROM PUBLIC CHANNELS**\n-Send direct message/video link to clone it here.\n\n🚨`NOTE:-` Our bot does not support \nPRIVATE CHANNEL/GROUP.\n\nJoin for update:- @Save_Restricted_contentz\n🥂kindly /donate for service alive' 
     WAIT_MSG = """"<b>Processing ...</b>"""
     REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
-    ABOUT_TXT = """
-Hi 👋 ☺️ 
+    Donate = """
+Hi Bro , If this bot helped you , kindly Donate to keep this service alive. you can send any amount**\n20₹, 30₹, 50₹, 100₹\n\n🔘 Payment Methods : `aman-9298@paytm` \n aman-9298@paytm 
 """
