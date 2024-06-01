ad4d2d8e1e26ccce96149d93ad261bd474ff5faf
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-01 08:39:42 +0530

    Update database.py

--- main/plugins/database.py
+++ main/plugins/database.py
@@ -1,75 +1,36 @@
-# (c) @Rudraa
-
-import datetime
-import motor.motor_asyncio
+#(©)CodeXBotz
 
 
-class Database:
 
-    def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
 
-    def new_user(self, id):
-        return dict(
-            id=id,
-            join_date=datetime.date.today().isoformat(),
-            ban_status=dict(
-                is_banned=False,
-                ban_duration=0,
-                banned_on=datetime.date.max.isoformat(),
-                ban_reason=''
-            )
-        )
+import pymongo, os
+from main.plugins.config import DB_URI, DB_NAME
 
-    async def add_user(self, id):
-        user = self.new_user(id)
-        await self.col.insert_one(user)
 
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return True if user else False
+dbclient = pymongo.MongoClient(DB_URI)
+database = dbclient[DB_NAME]
 
-    async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
 
-    async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+user_data = database['users']
 
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
 
-    async def remove_ban(self, id):
-        ban_status = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason=''
-        )
-        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
 
-    async def ban_user(self, user_id, ban_duration, ban_reason):
-        ban_status = dict(
-            is_banned=True,
-            ban_duration=ban_duration,
-            banned_on=datetime.date.today().isoformat(),
-            ban_reason=ban_reason
-        )
-        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+async def present_user(user_id : int):
+    found = user_data.find_one({'_id': user_id})
+    return bool(found)
 
-    async def get_ban_status(self, id):
-        default = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason=''
-        )
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('ban_status', default)
+async def add_user(user_id: int):
+    user_data.insert_one({'_id': user_id})
+    return
 
-    async def get_all_banned_users(self):
-        banned_users = self.col.find({'ban_status.is_banned': True})
-        return banned_users
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+        
+    return user_ids
+
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
