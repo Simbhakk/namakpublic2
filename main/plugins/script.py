21ab2c51db277f87505dd289fbc193c5b263a457
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-01 09:07:51 +0530

    Update script.py

--- main/plugins/script.py
+++ main/plugins/script.py
@@ -4,13 +4,5 @@ class Translation(object):
     WAIT_MSG = """"<b>Processing ...</b>"""
     REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
     ABOUT_TXT = """
-╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
-├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
-├💮 **Language** : [Python 3.12.3](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
-╰───────────────⍟
+Hi 👋 ☺️ 
 """
