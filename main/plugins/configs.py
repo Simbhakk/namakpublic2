a7e2eee214f4cd6a875311032d0d8f7bbcef79f6
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-01 08:38:35 +0530

    Update config.py

--- main/plugins/configs.py
+++ main/plugins/configs.py
@@ -1,14 +1,6 @@
 import os
 
 
-class Config(object):
-	API_ID = int(os.environ.get("API_ID", ''))
-	API_HASH = os.environ.get("API_HASH",'')
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", 'Publicsave2')
-	
-	DATABASE_URL = os.environ.get("DATABASE_URL",'mongodb+srv://devilbot:Rajbot@cluster0.ldrbggy.mongodb.net/?retryWrites=true&w=majority')
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", '-1001888538520'))
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779").split()))
-	
+
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "Publicsave2")
