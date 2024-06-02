922cdc8d1ca8f4fb153e5cc785edac565f80b6e7
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-02 22:20:19 +0530

    Update config.py

--- main/plugins/config.py
+++ main/plugins/config.py
@@ -3,4 +3,4 @@ import os
 
 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://devilbot:Rajbot@cluster0.ldrbggy.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "Publicsave2")
+DB_NAME = os.environ.get("DATABASE_NAME", "Publicsave3")
